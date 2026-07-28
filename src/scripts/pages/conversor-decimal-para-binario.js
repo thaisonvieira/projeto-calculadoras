534f56624efe2e2c67691b4e492cd4262cfa47b8
Thaison <thaisonvieirabr@gmail.com>
2026-07-28 16:14:15 -0300

    style: modifica o estilo do conversor de decimal para binário.

--- src/scripts/pages/conversor-decimal-para-binario.js
+++ src/scripts/pages/conversor-decimal-para-binario.js
@@ -1,18 +1,18 @@
 const form = document.querySelector('#formulario');
-const output = document.querySelector('#resultado');
+const resultado = document.querySelector('#resultado');
 
 const converterDecimalParaBinario = (num) => {
     if (num < 1) return;
     converterDecimalParaBinario(parseInt(num/2));
-    output.innerText += num % 2;
+    resultado.innerText += num % 2;
 }
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    const input = e.target.querySelector('#numero_em_decimal');
+    const input = e.target.querySelector('#numeroEmDecimal');
     const numeroEmDecimal = parseInt(input.value);
     
-    output.innerText = '';
+    resultado.innerText = '';
     converterDecimalParaBinario(numeroEmDecimal);
 });
