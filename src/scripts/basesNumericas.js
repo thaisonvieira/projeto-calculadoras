65772d5ae9c274d0c8b9918068c8ecfcedc1ca39
Thaison <thaisonvieirabr@gmail.com>
2026-08-20 16:44:12 -0300

    style: altera paleta de cores do site.

--- src/scripts/basesNumericas.js
+++ src/scripts/basesNumericas.js
@@ -1,4 +1,9 @@
-/* Função que converte um numero decimal para binário */
+/**
+ * Converte um número decimal em binário
+ * 
+ * @param {number} numero - numero decimal a ser convertido
+ * @returns {string} - numero em binario
+ */
 const decimalParaBinario = (numero) => {
     if (numero < 2) return String(numero);
     return decimalParaBinario(Math.floor(numero / 2)) + (numero % 2);
